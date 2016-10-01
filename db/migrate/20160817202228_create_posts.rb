7a549155b6b1240740e936befef0c51cc10bd220
Yasmin Benatti <yasmin.benatti@ringa.com.br>
2016-10-01 17:03:19 -0300

    adiciona ip listener

--- db/migrate/20160817202228_create_posts.rb
+++ db/migrate/20160817202228_create_posts.rb
@@ -3,7 +3,6 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
     	t.string :description
     	t.string :image
-    	t.datetime :date
     	t.integer :likes
     	t.belongs_to :user
 
